a40cf413c12560acc8dd24e25b75f884d5795203
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-10-03 08:29:41 +0300

    Setup content view

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,20 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock!")
-                .font(.title)
-                .foregroundColor(.green)
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
 
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock!")
+                    .font(.title)
+                    .foregroundColor(.green)
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
                     .font(.subheadline)
             }
+            .padding()
+
+            Spacer()
         }
-        .padding()
     }
 }
 
