3305c4a2f2e108d131df2235cf5833910e50c9fe
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-10-03 07:27:56 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sergey Lukaschuk on 03.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
